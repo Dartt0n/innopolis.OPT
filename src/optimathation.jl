d1a5fbf0b3e0ad46fb54b1217a5992001224132a
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-09-21 18:14:08 +0300

    feat: add main function

--- src/optimathation.jl
+++ src/optimathation.jl
@@ -1,5 +1,14 @@
 module optimathation
 
-greet() = print("Hello World!")
+function simplex(c, A, b)
+    table = initalTable(c, A, b)
+
+    while (improvementPossible(table))
+        pivot = findPivot(table)
+        applyPivot(table, pivot)
+    end
+
+    return simplexSolution(table)
+end
 
 end # module optimathation
