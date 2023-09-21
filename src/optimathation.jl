70dcca2ad3137eccfa6448ea0d57313c7197138c
PodYapolsky <podyapolskiyaa@gmail.com>
2023-09-21 19:06:00 +0300

    feat: improvementPossible function

--- src/optimathation.jl
+++ src/optimathation.jl
@@ -1,5 +1,14 @@
 module optimathation
 
+function improvementPossible(table::Matrix{Float64})
+    for i in 1:(size(table, 2) - 1)
+        if (table[1, i] < 0)
+            return true
+        end
+    end
+    return false
+end
+
 function simplex(c, A, b)
     table = initalTable(c, A, b)
 
