27cd104ef975fd3d5ed8d6350c5036abb35cea36
Tedor49 <66864989+Tedor49@users.noreply.github.com>
2023-09-21 18:53:13 +0300

    added initialTeable function

--- src/optimathation.jl
+++ src/optimathation.jl
@@ -11,6 +11,10 @@ function simplex(c, A, b)
     return simplexSolution(table)
 end
 
+function initialTable(C, A, b)
+    hcat(vcat([-C[i] for i=1:size(C,1)]', A), vcat(zeros(1, size(A, 1)), [convert(Float64, i==j) for i=1:size(A, 1), j=1:size(A, 1)]), vcat([0], b))
+end
+
 function simplexSolution(table::Matrix{Float64})
     _, m = size(table)
 
