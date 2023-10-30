2402735d6b478c5e6d432146c58ded88a29794fa
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-10-30 18:43:41 +0300

    feat: user prompts and output

--- src/InteriorPoint.jl
+++ src/InteriorPoint.jl
@@ -1,17 +1,19 @@
 module InteriorPoint
-
-using LinearAlgebra
-
 export interiorPoint
 
+using LinearAlgebra
+using Printf
+
+
 function interiorPoint(
-    alpha::Float64,
+    α::Float64,
     A::Matrix{Float64},
     C::Vector{Float64},
     X::Vector{Float64},
-    prec::Float64
+    ε::Float64
 )
     n = size(X, 1) # get 1st dimension of x
+    iteration = 1
 
     while true
         D = diagm(X) # convert vector x to diagonal matrix, where elements of x are diagonal elements
@@ -22,26 +24,31 @@ function interiorPoint(
         P = I(n) - waveA' * inv(waveA * waveA') * waveA
         Cp = P * waveC
 
-        mu, _ = findmin(x -> x < 0 ? x : Inf, Cp) # find the most negative element
 
-        if mu === Inf
+        μ, _ = findmin(x -> x < 0 ? x : Inf, Cp) # find the most negative element
+
+        if μ === Inf
             return X # most negative not found => no more negative elements
         end
 
         # by the formula mu = |min(Cp)|, but since mu is always negative, we can simply add -1
         # before mu in the formula below
-        waveX = ones(n) - alpha / mu * Cp
+        waveX = ones(n) - α / μ * Cp
 
         oldX = X # save previous value
         X = vec(D * waveX) # transform 4x1 matrix to vector with len=4
 
+        @printf "Iteration %03d X=" iteration
+        show(stdout, round.(X, digits=Int(abs(log10(ε)))))
+        println()
 
-        if norm(X - oldX) < prec
-            break # while difference is bigger then precision
+
+        if norm(X - oldX) < ε
+            return X # while difference is bigger then precision
         end
-    end
 
-    return X
+        iteration += 1
+    end
 end
 
 
--- src/main.jl
+++ src/main.jl
@@ -1,10 +1,50 @@
 include("./InteriorPoint.jl")
 using .InteriorPoint
 
-C = [1.0; 2.0; 0.0]
-A = [1.0 1.0 1.0;]
-# b is not needed
 
-initX = [2.0; 2.0; 4.0]
+function Base.parse(::Type{Vector{Float64}}, ss::Vector{String})::Vector{Float64}
+    return [parse(Float64, x) for x ∈ ss]
+end
 
-display(interiorPoint(0.5, A, C, initX, 0.01))
+function Base.parse(::Type{Matrix{Float64}}, ss::Vector{String})::Matrix{Float64}
+    return permutedims(hcat([parse(Vector{Float64}, Vector{String}(split(x, " "))) for x ∈ ss]...))
+end
+
+println("Enter N - number of variables:")
+N = parse(Int, readline())
+@show N
+
+println("Enter C - vector of coeficients (N✖1):")
+strings = [readline() for _ ∈ 1:N]
+C = parse(Vector{Float64}, strings)
+@show C
+
+println("Enter M - number of constrains:")
+M = parse(Int, readline())
+@show M
+
+println("Enter A - matrix of coeficients (M✖N):")
+strings = [readline() for _ ∈ 1:M]
+A = parse(Matrix{Float64}, strings)
+@show A
+
+if size(A) ≠ (M, N)
+    println("Matrix A must have dimensions of M✖N")
+    exit(1)
+end
+
+println("Enter X₀ - initial solution (N✖1):")
+strings = [readline() for _ ∈ 1:N]
+X₀ = parse(Vector{Float64}, strings)
+@show X₀
+
+println("Enter α - step size:")
+α = parse(Float64, readline())
+@show α
+
+println("Enter ε - required precision:")
+ε = parse(Float64, readline())
+@show ε
+
+println("Solution is:")
+@show interiorPoint(α, A, C, X₀, ε)
