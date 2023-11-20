5ee7badb95ec38505a21fe46420df9e7c66b90f9
Ivan Sannikov <87535782+Petrel321@users.noreply.github.com>
2023-11-21 00:56:48 +0300

    fix: Russel

--- src/Russel.jl
+++ src/Russel.jl
@@ -1,12 +1,14 @@
 module Russel # Russell’s approximation method
+export RusselApproximation
+
     function maxDeltaPos(Deltas::Matrix{Float64})
         row, col = (1, 1)
-        mx = abs(Deltas[row][col])
+        mx = abs(Deltas[row, col])
         i, j = 1, 1
         for row in 1:size(Deltas, 1)
             for col in 1:size(Deltas, 2)
-                if mx < abs(Deltas[row][col])
-                    mx = abs(Deltas[row][col])
+                if mx < abs(Deltas[row, col])
+                    mx = abs(Deltas[row, col])
                     i, j = row, col
                 end
             end
@@ -14,79 +16,84 @@ module Russel # Russell’s approximation method
         return i, j
     end
 
-    function clearDeltasRow(Deltas::Matrix{Float64}, row_idx::Int)
+    function clearDeltasRow(Deltas::Matrix{Float64}, row_idx)
         col = 1
-        while (col < size(Deltas, 2))
-            Deltas[row_idx][col] = 0
+        while (col <= size(Deltas, 2))
+            Deltas[row_idx, col] = 0
+            col += 1
         end
         return Deltas
     end
 
-    function clearDeltasCol(Deltas::Matrix{Float64}, col_idx::Int)
+    function clearDeltasCol(Deltas::Matrix{Float64}, col_idx)
         row = 1
-        while (row < size(Deltas, 1))
-            Deltas[row][col_idx] = 0
+        while (row <= size(Deltas, 1))
+            Deltas[row, col_idx] = 0
+            row += 1
         end
+
         return Deltas
     end
 
-    function maxRowElem(Cost::Matrix{Float64}, row_idx::Int)
+    function maxRowElem(Cost::Matrix{Float64}, row_idx)
         col = 1
-        mx = Cost[row_idx][col]
-        while (col < size(Cost, 2))
-            if mx < Cost[row_idx][j]
-                mx = Cost[row_idx][j]
+        mx = Cost[row_idx, col]
+        while (col <= size(Cost, 2))
+            if mx < Cost[row_idx, col]
+                mx = Cost[row_idx, col]
             end
             col += 1
         end
+
         return mx
     end
 
-    function fillU(Cost::Matrix{Float64}, u_len::Int)
+    function fillU(Cost::Matrix{Float64}, u_len)
         row = 1
-        U = Array{Float64}(0, u_len)
-        while (row < u_len)
+        U = Vector{Float64}(undef, u_len)
+        while (row <= u_len)
             U[row] = maxRowElem(Cost, row)
             row += 1
         end
+
         return U
     end
 
-    function maxColumnElem(Cost::Matrix{Float64}, col_idx::Int)
+    function maxColumnElem(Cost::Matrix{Float64}, col_idx)
         row = 1
-        mx = Cost[row][col_idx]
-        while (row < size(Cost, 1))
-            if mx < Cost[row][col_idx]
-                mx = Cost[row][col_idx]
+        mx = Cost[row, col_idx]
+        while (row <= size(Cost, 1))
+            if mx < Cost[row, col_idx]
+                mx = Cost[row, col_idx]
             end
             row += 1
         end
         return mx
     end
 
-    function fillV(Cost::Matrix{Float64}, v_len::Int)
+    function fillV(Cost::Matrix{Float64}, v_len)
         col = 1
-        V = Array{Float64}(0, v_len)
-        while (col < v_len)
+        V = Vector{Float64}(undef, v_len)
+        while (col <= v_len)
             V[col] = maxColumnElem(Cost, col)
             col += 1
         end
         return V
     end
 
-    function createDeltas(Cost::Matrix{Float64}, u_len::Int, v_len::Int)
-        Deltas = Array{Float64}(0, u_len, v_len)
+    function createDeltas(Cost::Matrix{Float64}, u_len, v_len)
+        Deltas = Matrix{Float64}(undef, u_len, v_len)
         U = fillU(Cost, u_len)
         V = fillV(Cost, v_len)
         for row in 1:u_len
             for col in 1:v_len
-                Deltas[row][col] = Cost[row][col] - U[row] - V[col]
+                Deltas[row, col] = Cost[row, col] - U[row] - V[col]
             end
         end
         return Deltas
     end
 
-    function improvementPossible(Supply::Vector{Float64}, u_len::Int)
+    function improvementPossible(Supply::Vector{Float64}, u_len)
         for row in 1:u_len
             if Supply[row] != 0
                 return true
@@ -108,8 +115,8 @@ module Russel # Russell’s approximation method
         while (improvementPossible(Supply, u_len))
             i_mx, j_mx = maxDeltaPos(Deltas)
             mn = min(Demand[j_mx], Supply[i_mx])
-            answer += Cost[i_mx][j_mx] * mn
-            
+            answer += Cost[i_mx, j_mx] * mn
+
             Demand[j_mx] -= mn
             Supply[i_mx] -= mn
 
@@ -121,5 +128,8 @@ module Russel # Russell’s approximation method
             end
 
         end
+
+        return answer
     end
+    
 end
