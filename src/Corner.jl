b5881465fa94d36f980f7c2f289c7d560eeb444f
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-11-20 23:17:18 +0300

    fix: corner

--- src/Corner.jl
+++ src/Corner.jl
@@ -4,26 +4,25 @@ function NorthWestCorner(
     Cost::Matrix{Float64},
     Demand::Vector{Float64},
     Supply::Vector{Float64}
-)
+)::Vector{Tuple{Int,Int,Float64}}
     row, col = (1, 1)
-    answer = 0
+    answer = []
 
-    while (row < size(A, 1) && col < size(A, 2))
-        answer += Cost[row, col]
+    while (row < size(Cost, 1) && col < size(Cost, 2))
+        min_value = min(Supply[row], Demand[col])
+        append!((row, col, Cost[row, col] * min_value))
+        Supply[row] -= min_value
+        Demand[col] -= min_value
 
-
-        if Cost[row, col] < Supply[row]
-            Supply[row] -= Demand[col]
-
+        if Supply[row] != 0
             col += 1
         else
-            Demand[col] -= Supply[row]
-
             row += 1
         end
+
     end
 
-
+    return answer
 end
 
 end
