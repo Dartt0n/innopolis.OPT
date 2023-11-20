710033196c5c6352c578c18beb7c1db69f7050f3
Ivan Sannikov <87535782+Petrel321@users.noreply.github.com>
2023-11-21 01:09:20 +0300

    fix: refix Corner

--- src/Corner.jl
+++ src/Corner.jl
@@ -1,26 +1,28 @@
 module Corner # North-West cornel method
 
-    function NorthWestCorner(
-        Cost::Matrix{Float64},
-        Demand::Vector{Float64},
-        Supply::Vector{Float64}
-    )
-        row, col = (1, 1)
-        answer = 0
+function NorthWestCorner(
+    Cost::Matrix{Float64},
+    Demand::Vector{Float64},
+    Supply::Vector{Float64}
+)::Vector{Tuple{Int,Int,Float64}}
+    row, col = (1, 1)
+    answer = []
 
-        while (row < size(Cost, 1) && col < size(Cost, 2))
-            answer += Cost[row, col]
+    while (row < size(Cost, 1) && col < size(Cost, 2))
+        min_value = min(Supply[row], Demand[col])
+        append!((row, col, Cost[row, col] * min_value))
+        Supply[row] -= min_value
+        Demand[col] -= min_value
 
-            if Cost[row, col] < Supply[row]
-                Supply[row] -= Demand[col]
-                col += 1
-            else
-                Demand[col] -= Supply[row]
-                row += 1
-            end
+        if Supply[row] != 0
+            col += 1
+        else
+            row += 1
         end
 
-
     end
+
+    return answer
+end
 
 end
