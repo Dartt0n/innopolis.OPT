ea6db4ecd284ada9873c95e98c9d63979a082744
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-11-21 19:47:11 +0300

    test: russel and vogel

--- test/test_russel.jl
+++ test/test_russel.jl
@@ -5,11 +5,19 @@ using .Russel
 
 @testset "Testing Russel" begin
     # Lab 7, Problem 3
-    C::Matrix{Float64} = [7.0 8.0 1.0 2.0; 4.0 5.0 9.0 8.0; 9.0 2.0 3.0 6.0]
+    C::Matrix{Float64} = [
+        7.0 8.0 1.0 2.0
+        4.0 5.0 9.0 8.0
+        9.0 2.0 3.0 6.0
+    ]
     D::Vector{Float64} = vec([120.0 50.0 190.0 110.0])
     S::Vector{Float64} = vec([160.0 140.0 170.0])
 
-    answer = 1530
+    answer = Matrix{Union{Float64,Nothing}}([ # todo: check this test
+        nothing nothing 160.0 nothing
+        120.0 nothing nothing 20.0
+        nothing 50.0 30.0 90.0
+    ])
 
     @test RusselApproximation(C, D, S) == answer
 
--- test/test_vogel.jl
+++ test/test_vogel.jl
@@ -5,19 +5,32 @@ using .Vogel
 
 @testset "Testing Vogel" begin
     # Lab 7, Problem 3
-    C::Matrix{Float64} = [7.0 8.0 1.0 2.0; 4.0 5.0 9.0 8.0; 9.0 2.0 3.0 6.0]
+    C::Matrix{Float64} = [
+        7.0 8.0 1.0 2.0
+        4.0 5.0 9.0 8.0
+        9.0 2.0 3.0 6.0
+    ]
     D::Vector{Float64} = vec([120.0 50.0 190.0 110.0])
     S::Vector{Float64} = vec([160.0 140.0 170.0])
 
     # answer
-    s::Float64 = 1330.0
+    answer = Matrix{Union{Float64,Nothing}}([
+        nothing nothing 50.0 110.0
+        120.0 20.0 nothing nothing
+        nothing 30.0 140.0 nothing
+    ])
 
     # check sums
     @test sum(D) == sum(S)
-    
+
     # checks dimensions
     @test size(C) == (length(S), length(D))
-    
+
+    get = vogel(C, D, S)
+
+    @show answer
+    @show get
+
     # gauge the function itself
-    @test isapprox(vogel(C, D, S), s)
+    @test get == answer
 end
